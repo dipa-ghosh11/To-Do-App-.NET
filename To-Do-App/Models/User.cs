8df3f55d5bf343ca4a12a2187892ac48fcd5672b
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-01 22:44:26 +0530

    User model added

--- To-Do-App/Models/User.cs
+++ To-Do-App/Models/User.cs
@@ -1,10 +1,35 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 namespace To_Do_App.Models;
 
 public class User
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public required string Id { get; set; }
 
+
+    [BsonElement("name")]
+    [Required]
+    [StringLength(50)]
+    public required string Name { get; set; }
+
+    [BsonElement("email")]
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+
+    [BsonElement("password")]
+    [Required]
+    [MinLength(8)]
+    public required string Password { get; set; }
+
+    [BsonElement("phone")]
+    [Required]
+    [Range(1000000000, 9999999999)]
+    public int Phone { get; set; }
 }
