f433f1b1a1eead9c7f6599b7c8fc124710210c16
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-01 11:22:34 +0530

    MongoDB driver added

--- To-Do-App/Models/User.cs
+++ To-Do-App/Models/User.cs
@@ -1,5 +1,7 @@
 using System;
-
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 namespace To_Do_App.Models;
 
 public class User
--- To-Do-App/obj/Debug/net9.0/To-Do-App.AssemblyInfo.cs
+++ To-Do-App/obj/Debug/net9.0/To-Do-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da7718b8bdda99b91fc4da23d5c0bcae661f6c72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ad6ae26d7e8c1174981202643123bae5b0b2ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
