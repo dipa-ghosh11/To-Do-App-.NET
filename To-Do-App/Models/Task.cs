b2f8583f90950d062eff9b3a9393f894142329b3
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-01 22:58:16 +0530

    Task model added

--- To-Do-App/Models/Task.cs
+++ To-Do-App/Models/Task.cs
@@ -1,8 +1,39 @@
 using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace To_Do_App.Models;
 
 public class Task
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
 
+
+
+    [BsonElement("title")]
+    public required string Title { get; set; }
+
+
+
+    [BsonElement("description")]
+    public string? Description { get; set; }
+
+
+    [BsonElement("status")]
+    public required string Status { get; set; }
+
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+
+
+    [BsonElement("user")]
+    public required string User { get; set; }
+    
 }
