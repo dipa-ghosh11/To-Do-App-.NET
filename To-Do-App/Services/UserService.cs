79f430ff40fa82d94cddbf826bd008a44a9ea3a6
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-02 22:16:36 +0530

    User service code

--- To-Do-App/Services/UserService.cs
+++ To-Do-App/Services/UserService.cs
@@ -1,8 +1,14 @@
 using System;
+using MongoDB.Driver;
+using To_Do_App.Models;
 
 namespace To_Do_App.Services;
 
 public class UserService
 {
+    private readonly IMongoCollection<User> _users;
 
+    public UserService(ToDoDatabaseSettings settings){
+
+    }
 }
--- To-Do-App/obj/Debug/net9.0/To-Do-App.AssemblyInfo.cs
+++ To-Do-App/obj/Debug/net9.0/To-Do-App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-Do-App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2f8583f90950d062eff9b3a9393f894142329b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a1a35285066216397ad805e5d7dc0b52ada7f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-Do-App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-Do-App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
