6d2d4394241c90aba959e40022236effdb98044a
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-05 12:56:02 +0530

    Task controller updated

--- To-Do-App/Controllers/TaskController.cs
+++ To-Do-App/Controllers/TaskController.cs
@@ -1,8 +1,23 @@
 using System;
+using Microsoft.AspNetCore.Mvc;
+using To_Do_App.Models;
+using To_Do_App.Services;
 
-namespace To_Do_App.Controllers;
 
-public class TaskController
-{
+namespace To_Do_App.Controllers{
+
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private readonly TaskService _taskService;
+
+        public TaskController(TaskService taskService){
+            _taskService = taskService;
+        }
+
+        
+    }
 
 }
