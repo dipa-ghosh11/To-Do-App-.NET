6d3b32a0102129ca48f61a999e9235bfbe4f7386
Dipa-44 <dipaghosh0902@gmail.com>
2025-05-09 20:05:43 +0530

    claim added

--- To-Do-App/Utils/TokenUtils.cs
+++ To-Do-App/Utils/TokenUtils.cs
@@ -4,10 +4,15 @@ using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using To_Do_App.Models;
+using Microsoft.VisualBasic;
 namespace To_Do_App.Utils;
 
 
-public class JwtHelper
+public class TokenUtil
 {
+     var claims= new[]
+     {
+        new Claim(JwtRegisteredClaimNames.Sub, config["Jwt:Subject"]),
 
+     }
 }
